fee24733c997ff04101a2e119a6e21bae59c8bc2
Václav Vondráček <reamless@seznam.cz>
2024-04-17 07:59:41 +0200

    new login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,18 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import "./navbar.css";
+
 function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-success sticky-top">
+    <Navbar expand="lg" className="sticky-top bg-transparent">
       <Container>
         <Navbar.Brand href="#home" className="text-light">
           Krasino
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto justify-content-center">
             <Nav.Link href="#home" className="text-light">
               Home
             </Nav.Link>
